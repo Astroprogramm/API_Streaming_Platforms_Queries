8598de74cb202ac39441e1d7ab3d82bfcc68bd22
Astroprogramm <aguirrealvareza97@gmail.com>
2022-12-11 10:29:10 -0500

    updates

--- FastAPI_Docker/main.py
+++ FastAPI_Docker/main.py
@@ -2,10 +2,21 @@ from fastapi import FastAPI, Path, UploadFile, File
 from typing import Optional
 from pydantic import BaseModel
 import shutil
+import pandas as pd
 
 app=FastAPI()
 
-@app.get('/get_max_duration({year},{platform},{type})')
-async def get_max_duration():
-    return {'name':'First Data'}
+Data_file = UploadFile('Data.json')
+
+@app.on_event('startup')
+async def startup():
+	global DATA
+	DATA = pd.read_json('Data.json')
+	
+#{year},{platform},{type}
+@app.get('/get_max_duration/{year},{platform},{min_season}')
+async def get_max_duration(year:int,platform:str,min_season:str):
+	id_max_duration = DATA[(DATA.release_year==year) | (DATA.Platform==platform) | (DATA.duration_type==min_season)]
+	max_title= id_max_duration.title.loc[id_max_duration['duration_quantity'].idxmax()]
+	return max_title
 
